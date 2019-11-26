f83d856a45a415ac427f00e635976da89cc0a2c0
SRS <sirusheng@agora.io>
2019-11-26 12:28:42 +0800

    add pipelines setting

---
+++ Agora-Video-With-SenseTime-iOS/Agora-With-SenseTime/KeyCenter.m
@@ -0,0 +1,29 @@
+//
+//  KeyCenter.m
+//  Agora-With-SenseTime
+//
+//  Created by SRS on 2019/11/26.
+//  Copyright © 2019 agora. All rights reserved.
+//
+
+#import "KeyCenter.h"
+
+@implementation KeyCenter
+
+-(NSString*)agoraAppId {
+    return <#Agora App Id#>;
+}
+
+-(NSString*)agoraAppToken {
+    return <#Agora App Token#>;
+}
+
+-(NSString*)senseAppId {
+    return <#Sense App Id#>;
+}
+
+-(NSString*)senseAppKey {
+    return <#Sense App Key#>;
+}
+
+@end
